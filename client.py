99c0e4b73b203a614d373e5a615557ee82b8b9da
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2022-08-16 02:47:03 -0600

    Show Specific Info and Delete User Functions

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
-from getpass import getpass
 from argparse import ArgumentParser
 import slixmpp
 import sys
 import asyncio
 from slixmpp.exceptions import IqError, IqTimeout
+from slixmpp.xmlstream.stanzabase import ET 
 import threading
 
 if sys.platform == 'win32' and sys.version_info >= (3, 8):
@@ -29,26 +29,34 @@ class Show_Users(slixmpp.ClientXMPP):
         self.presences.wait(3)
 
         groups = self.client_roster.groups()
+        print('\033[0;36;40m')
         for group in groups:
-            print('\033[1;31;40m')
-            print('\n%s' % group)
-            print('-' * 50 + 'USERS LIST' + '-' * 50)
-            for jid in groups[group]:
-                name = self.client_roster[jid]['name']
-                if self.client_roster[jid]['name']:
-                    print(' %s (%s)' % (name, jid))
-                else:
-                    print('\n',jid)
+            for contact in groups[group]:
+                connections = self.client_roster.presence(contact)
 
-                connections = self.client_roster.presence(jid)
+                username = 'None'
+                status = 'None'
+                show = 'None'
+                extra = 'None'
+
+                if self.client_roster[contact]['name']:
+                    username = self.client_roster[contact]['name']
+
                 for res, pres in connections.items():
-                    show = 'available'
                     if pres['show']:
                         show = pres['show']
-                    print('   - %s (%s)' % (res, show))
                     if pres['status']:
-                        print('       %s' % pres['status'])
-                        print('-' * 72)
+                        status = pres['status']
+                    
+                    extra = res
+                
+                print("User Mail: ", contact)
+                print("Username: ", username)
+                print("Status: ", status)
+                print("Show: ", show)
+                print("Extra: ", extra)
+                
+                print('\n')
         print('\033[0;37;40m')
         self.disconnect()
 
@@ -79,3 +87,140 @@ class Send_Private_Message(slixmpp.ClientXMPP):
           await self.get_roster()
           self.send_message(mto=self.recipient,mbody=self.msg,mtype='chat')
           self.disconnect()
+
+class Show_User(slixmpp.ClientXMPP):
+    def __init__(self, jid, password, friend):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.start)
+        self.presences = threading.Event()
+        self.friend = friend
+
+    async def start(self, event):
+        self.send_presence()
+        await self.get_roster()
+    
+        try:
+            self.get_roster()
+        except IqError as e:
+            print("Error", e)
+        except IqTimeout:
+            print("Server Timeout")
+        
+        self.presences.wait(3)
+        response = []
+
+        groups = self.client_roster.groups()
+        for group in groups:
+            for contact in groups[group]:
+                if self.friend == contact:
+                    connections = self.client_roster.presence(contact)
+
+                    username = status = show = extra = 'None'
+
+                    if self.client_roster[contact]['name']:
+                        username = self.client_roster[contact]['name']
+
+                    for res, pres in connections.items():
+                        if pres['show']:
+                            show = pres['show']
+                        if pres['status']:
+                            status = pres['status']
+                        
+                        extra = res
+                    
+                    response.append("User Mail: " + contact)
+                    response.append("Username: " + username)
+                    response.append("Status: " + status)
+                    response.append("Show: " + show)
+                    response.append("Extra: " + extra)
+        
+        if len(response) > 0:
+            for data in response:
+                print('\033[0;36;40m')
+                print(data)
+        else:
+            print('\033[1;31;40m')
+            print("The user doesn't exist in your list of contacts")
+
+        print('\033[0;37;40m')
+        self.disconnect()
+
+class Change_Presence(slixmpp.ClientXMPP):
+    def __init__(self, jid, password, information, status):
+          slixmpp.ClientXMPP.__init__(self, jid, password)
+          self.information = information
+          self.status = status
+          self.jid = jid
+          self.add_event_handler("session_start", self.start)
+
+    async def start(self, event):
+        self.send_presence(pstatus="Available")
+        await self.get_roster()
+        self.disconnect()
+    
+class Delete_Account(slixmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        self.user = jid
+        self.add_event_handler("session_start", self.start)
+
+    async def start(self, event):  
+        self.send_presence()
+        await self.get_roster()
+
+        delete = self.Iq()
+        delete['type'] = 'set'
+        delete['from'] = self.user
+        fragment = ET.fromstring("<query xmlns='jabber:iq:register'><remove/></query>")
+        delete.append(fragment)
+
+        try:
+            delete.send()
+            print('\033[1;31;40m')
+            print("Deleted Account")
+
+        except IqError as e:           
+            print("Error", e)
+
+        except IqTimeout:
+            print("Server Timeout")
+
+        except Exception as e:     
+            print(e)  
+
+        self.disconnect()
+
+class Register_Account(slixmpp.ClientXMPP):
+    def __init__(self, jid, password, mail):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+        
+        self.user = jid
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("register", self.register)
+    
+    def start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+    def register(self, iq):  
+        create = self.Iq()
+        create['type'] = 'set'
+        fragment = ET.fromstring("<query xmlns='jabber:iq:register'><username>Hekkice</username><password>1213</password><email>cutci7vu@alumchat.fun</email></query>")
+        create.append(fragment)
+        create.send()
+        try:
+            #create.send()
+            print('\033[1;31;40m')
+            print("New Account Created")
+
+        except IqError as e:           
+            print("Error", e)
+
+        except IqTimeout:
+            print("Server Timeout")
+
+        except Exception as e:     
+            print(e)  
+
+        self.disconnect()
--- menu.py
+++ menu.py
@@ -13,9 +13,12 @@ def second_menu():
 
     print('\033[1;37;40m')
 
-    print('\n 1) Show User List')
-    print('\n 2) Add User as Friend')
-    print('\n 3) Send Message \n')
+    print('\n 1) Show Users List')
+    print('\n 2) Show User Information')
+    print('\n 3) Add User as Friend')
+    print('\n 4) Send Message ')
+    print('\n 5) Define Presence Message ')
+    print('\n 6) Delete Account ')
     print('\n !) Log Out \n')
 
     print('\033[0;37;40m')
@@ -35,19 +38,15 @@ while(exit1):
     print('\033[0;37;40m')
     option1 = int(input("Write your option number: "))
     if(option1 == 1):
-        user = input("Username: ")
+        username = input("Username: ")
         password = input("Password: ")
-        #xmpp = Client(user+"@alumchat.fun",password)
-        xmpp.register_plugin('xep_0004') # Data forms
-        xmpp.register_plugin('xep_0066') # Out-of-band Data
-        xmpp.register_plugin('xep_0077')
-        xmpp.register_plugin('xep_0085')
-        xmpp['xep_0077'].force_registration = True
+        email = input("Mail: ")
+        xmpp = Register_Account(username, password, email)
         xmpp.connect()
         xmpp.process()
     elif(option1 == 2):
-        username = input("Username: ")
-        password = input("Password: ")
+        username = 'dwdwd@alumchat.fun'
+        password = '123'
         exit2 = True
         while(exit2):
             second_menu()
@@ -59,19 +58,38 @@ while(exit1):
                 xmpp.process(forever=False)
             
             elif(option2 == 2):
+                friend = input("Name of contact you want to get information: ")
+                xmpp = Show_User(username, password, friend)
+                xmpp.connect()
+                xmpp.process(forever=False)
+            
+            elif(option2 == 3):
                 friend = input("Name of contact you want to add: ")
                 xmpp = Add_User(username, password, friend)
                 xmpp.connect()
                 xmpp.process(forever=False)
             
-            elif(option2 == 3):
+            elif(option2 == 4):
                 friend = input("Name of contact you want to send the message: ")
                 message = input("Message: ")
                 xmpp = Send_Private_Message(username, password, friend, message)
                 xmpp.connect()
                 xmpp.process(forever=False)
             
-            elif(option2 == 4):
+            elif(option2 == 5):
+                information = input("Information you want to add to your status: ")
+                status = input("Status: ")
+                xmpp = Change_Presence(username, password, information, status)
+                xmpp.connect()
+                xmpp.process(forever=False)
+            
+            if(option2 == 6):
+                xmpp = Delete_Account(username, password)
+                xmpp.connect()
+                xmpp.process(forever=False)
+                exit2 = False
+            
+            elif(option2 == 7):
                 exit2 = False
 
             else:
